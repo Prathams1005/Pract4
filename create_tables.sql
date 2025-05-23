f7eea74330f9db82c0d1f0396584235fa5dfe491
college1 <college1@example.com>
2025-05-23 15:38:33 +0530

    Third3

--- create_tables.sql
+++ create_tables.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS demo;
 
 USE demo;
 
-CREATE TABLE students(
+CREATE TABLE students (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
+    username VARCHAR(100),
     marks INT
 );
 
-INSERT INTO students (name,marks) VALUES
-("Pratham",85),
+INSERT INTO students (username, marks) VALUES
+('Pratham',85),
 ("Aditya",90),
-("Pranav",89),
+("Pranav",89);
